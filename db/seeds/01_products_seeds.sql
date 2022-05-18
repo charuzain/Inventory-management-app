b8d0fe02b7d2d16fbfc0e96956e7e3a7f781301e
charuzain <charuabhizain@gmail.com>
2022-05-17 23:58:24 -0400

    Data isnerted into product table

---
+++ db/seeds/01_products_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+products(name,price,description,quantity)
+VALUES
+ ('Hot Wheels','2','Hot Wheels Color Shifters Vehicle 5-Pack', 20),
+ ('Lego','200','700 piece lego ninjago set', 23),
+ ('Battleship','20','Classic Battleship game includes ships,red and white pegs', 40),
+ ('Game of Life','29.99','Includes gameboard,tokens,spinner ,cards, money pack', 90),
+ ('Clue','34.32','Classical clue boardgame for kids', 15),
