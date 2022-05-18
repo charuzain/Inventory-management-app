5603183ad879d64bae8b45a7bc14f08111ae95da
charuzain <charuabhizain@gmail.com>
2022-05-18 00:08:52 -0400

    Seeded warehouse table

---
+++ db/seeds/02_warehouses_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO warehouses 
+(name,address,city,state,zip,country)
+VALUES
+ ('Toy4Kids','33 Lillian street','Whitby','Ontario','L2C1M2','Canada'),
+ ('SmallWonder','20 Havenbrook','Calgary','Alberta','1M1A0P','Canada'),
+ ('JrWorld','40 Lincon Dr','Plano','Texas','A75024','USA'),
+ ('LittleWorld','334 Belleuve Dr','Seattle','Washington','SE50A9','USA'),
+ ('SmartKids','30 Jharsa Road','Gurgaon', 'Haryana','India'),
