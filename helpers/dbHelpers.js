1150a57363b8dd08ad1c3d07327cf3dd741dd9c4
charuzain <charuabhizain@gmail.com>
2022-05-19 18:14:02 -0400

    helper function to get products from db

---
+++ helpers/dbHelpers.js
@@ -0,0 +1,39 @@
+
+module.exports = (db) => {
+
+  const getAllProducts = () => {
+    const query = `SELECT prod.name, prod.description,prod.quantity , STRING_AGG(ware.name,', ') as      location from products prod 
+              JOIN product_warehouse pc on prod.id = pc.product_id
+              JOIN warehouses ware ON ware.id=pc.warehouse_id GROUP BY prod.name, prod.description,prod.quantity `;
+   
+    return db
+      .query(query)
+      .then((result) => {
+        console.log(result.rows);
+        return result.rows;
+      })
+      .catch((err) => err);
+  };
+
+  const getSingleProduct = (id) => {
+    const query = {
+      text: `SELECT * FROM products WHERE id = $1`,
+      values: [id],
+    };
+    return db
+      .query(query)
+      .then((result) => {
+        if (!result.rows[0]) {
+          return `Product with id ${id} not found `;
+        } else {
+          return result.rows[0];
+        }
+      })
+      .catch((err) => err);
+  };
+  return {
+    getAllProducts,
+    getSingleProduct,
+  
+  };
+};
