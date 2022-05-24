81a1c27eb6de4a519b27522ed05c941b1a7485ec
charuzain <charuabhizain@gmail.com>
2022-05-23 21:47:07 -0400

    route to fetch single warehouse info

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const router = express.Router();
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-module.exports = ({getAllWarehouses})=>{
+module.exports = ({getAllWarehouses,addNewWarehouse,getSingleWarehouse})=>{
 
    
 
@@ -12,16 +16,61 @@ module.exports = ({getAllWarehouses})=>{
     getAllWarehouses().then(warehouses => {
       console.log(warehouses);
       console.log({warehouses});
-      res.render('warehouse',{warehouses});
+      res.render('warehouses',{warehouses});
     });
+  });
+
+  router.get('/new',(req,res)=>{
+    res.render('new_warehouse');
+  });
 
 
-    router.get('/new',(req,res)=>{
-      res.render('new_warehouse');
+  router.get('/:id',(req,res)=>{
+    
+    const id = req.params.id;
+    // console.log(id);
+    getSingleWarehouse(id).then(warehouse =>{
+      res.render('single_warehouse',{warehouse});
     });
+    
+      
+      
+  });
+
+
+
+  router.post('/',(req,res)=>{
+
+    const name = req.body.name;
+    const address = req.body.address;
+    const city = req.body.city;
+
+    const state = req.body.state;
+
+    const country  = req.body.country;
+
+    const zip = req.body.zip;
+
+
+
+    addNewWarehouse(name , address , city , state , country , zip).then(value=>{
+      console.log(value);
+      console.log(`value is ${value}`);
+
+      res.redirect('/warehouses');
+       
+    });
+
+  });
   
     
-  });
+
+  
+
+    
+
+ 
+
 
 
 
