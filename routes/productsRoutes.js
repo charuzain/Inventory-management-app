ef4cc6793744413efee0ab249b3cfa7eee530d52
charuzain <charuabhizain@gmail.com>
2022-05-23 21:47:37 -0400

    route to update product info added

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -3,28 +3,29 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 
 
-
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-module.exports = ({getAllProducts,getSingleProduct,getProducts,getAllWarehouses,addNewProduct})=>{
+module.exports = ({getAllProducts,getSingleProduct,getAllWarehouses,addNewProduct,updateProduct})=>{
 
    
 
   router.get('/',(req,res)=>{
     getAllProducts().then(products =>{
-      // console.log(products[1].location);
-      // console.log({products});
+      console.log(products[1].location);
+      console.log({products});
       res.render('products' ,{products});
     });
   
     
   });
 
- 
 
+
+ 
+  // Create new product
 
   router.get('/new',(req,res)=>{
     getAllWarehouses().then(warehouses=>{
@@ -33,16 +34,65 @@ module.exports = ({getAllProducts,getSingleProduct,getProducts,getAllWarehouses,
    
   });
 
+  router.get('/:id',(req,res)=>{
+    
+    const id = req.params.id;
+    // console.log(id);
+    getSingleProduct(id).then(product =>{
+      // console.log(product);
+      res.render('single_product',{product});
+    });
+    
+      
+      
+  });
+
+
+  router.get('/:id/edit',(req,res)=>{
+    
+    const id = req.params.id;
+    console.log(id);
+    getSingleProduct(id).then(product =>{
+      getAllWarehouses().then(warehouses=>{
+        // console.log(warehouses);
+        res.render('edit_product' , {product , warehouses});
+      });
+    });
+  });
+
+  router.post('/:id',(req,res)=>{
+    const id = req.params.id;
+    const {name,description,price,totalquantity,location} = req.body;
+    getSingleProduct(id).then(response =>{
+      if (response) {
+        return updateProduct(name,description,price,totalquantity,location);
+      }
+
+
+    });
+
+  });
+    
+      
+      
+
+
+  
+
+
+
+
+  
  
   router.post('/',(req,res)=>{
-    // console.log(`name is ${req.body.name}`);
-    // console.log(req.body);
+    console.log(`name is ${req.body.name}`);
+    console.log(req.body);
     const name = req.body.name;
     const description = req.body.name;
     const price = req.body.price;
     const quantity = req.body.totalquantity;
     const location = req.body.location;
- 
+   
     addNewProduct(name,description,price,quantity,location).then(value=>{
       console.log(value);
       res.redirect('/products');
@@ -55,16 +105,16 @@ module.exports = ({getAllProducts,getSingleProduct,getProducts,getAllWarehouses,
 
 
 
-  router.get('/:id',(req,res)=>{
-    const id = req.params.id;
-    getSingleProduct(id)
-      .then((product)=>{
-        res.json(product);
-      })
-      .catch((err) => res.json({
-        error: err.message
-      }));
-  });
+  // router.get('/:id',(req,res)=>{
+  //   const id = req.params.id;
+  //   getSingleProduct(id)
+  //     .then((product)=>{
+  //       res.json(product);
+  //     })
+  //     .catch((err) => res.json({
+  //       error: err.message
+  //     }));
+  // });
 
 
 
